7a3e29a6deaade7b4438296bf0fab86daee62b47
Brennan Fulmer <brennan.flmr@gmail.com>
2018-05-16 04:33:10 +0000

    automatically backed up by learn

--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -38,7 +38,7 @@ describe 'collections practice' do
   
       describe '#swap_elements_from_to 2' do
     it 'swap the supplied elements of an array' do
-      expect(swap_elements_from_to(["a", "b", "c"],2,1)).to eq(["c", "b", "a"])
+      expect(swap_elements_from_to(["a", "b", "c"],2,1)).to eq(["a", "c", "b"])
     end
   end
 
